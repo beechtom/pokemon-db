3d98299fe1ab3667314f2a0a1904ee8d8399ea45
beechtom <30373575+beechtom@users.noreply.github.com>
2018-11-18 20:49:36 -0500

    Update pool.js

--- middlewares/pool.js
+++ middlewares/pool.js
@@ -2,10 +2,10 @@ var mysql = require('mysql');
 
 // Connect mySQL
 var pool  = mysql.createPool({
-    host    : 'classmysql.engr.oregonstate.edu',
-    user    : 'cs340_beecht',
-    password: '4518',
-    database: 'cs340_beecht',
+    host    : '*******************',
+    user    : '*******************',
+    password: '*******************',
+    database: '*******************',
     multipleStatements: true,
 });
 
